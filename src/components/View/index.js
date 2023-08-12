eeb398c720f38cf61ca334df257691a97c7b9d3f
Preeti <preetijav@gmail.com>
2023-08-12 19:29:20 +0200

    Added delete to view

--- src/components/View/index.js
+++ src/components/View/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { Box, Button, Flex, Header } from "../styled";
+import { deleteEmployee } from "../../redux/employees/actionCreators";
 
 const View = () => {
+  const employees = useSelector(state => state.employees.employees_records);
+  const dispatch = useDispatch();
   const history = useHistory();
+
+  const handleDelete = employeeId => {
+    dispatch(deleteEmployee(employeeId));
+  };
 
   return (
     <>
@@ -18,6 +26,43 @@ const View = () => {
           <Button data-cy="backButton" onClick={() => history.goBack()}>
             Back
           </Button>
+        </Box>
+        <Box marginTop="md">
+          <table>
+            <thead>
+              <tr>
+                <th>FirstName</th>
+                <th>Surname</th>
+                <th>Email</th>
+                <th>Birth Date</th>
+                <th>Status</th>
+                <th>Job Title</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees &&
+                employees.map(employee => (
+                  <tr key={employee.id}>
+                    <td>{employee.firstName}</td>
+                    <td>{employee.surname}</td>
+                    <td>{employee.email}</td>
+                    <td>{employee.birthDate}</td>
+                    <td>{employee.status}</td>
+                    <td>{employee.jobTitle}</td>
+                    <td>{employee.actions}</td>
+                    <td>
+                      <Button
+                        data-cy={`deleteButton-${employee.id}`}
+                        onClick={() => handleDelete(employee.id)}
+                      >
+                        Delete
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </Box>
       </Flex>
     </>
