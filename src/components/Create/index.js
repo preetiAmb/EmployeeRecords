daeaafdb9ba35e62f22abf35ddc0a5f4a85db9bd
Preeti <preetijav@gmail.com>
2023-08-13 22:36:32 +0200

    Added edit to create

--- src/components/Create/index.js
+++ src/components/Create/index.js
@@ -1,42 +1,78 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { Formik } from "formik";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Flex, Header } from "../styled";
 import FormField from "./FormField";
 import FormButtons from "./FormButtons";
 import formValidationSchema from "./formValidationSchema";
-import { saveNewEmployee } from "../../redux/employees/actionCreators";
+import {
+  saveNewEmployee,
+  editEmployee,
+  resetEmployeeToEdit,
+} from "../../redux/employees/actionCreators";
 
 const Create = () => {
+  const { employeeId } = useParams();
   const dispatch = useDispatch();
+  const employees = useSelector(state => state.employees.employees_records);
+
+  const employeeToEdit = useMemo(
+    () =>
+      employees.find(employee => employee.id === parseInt(employeeId, 10)) ||
+      {},
+    [employees, employeeId]
+  );
+
   const submitForm = useCallback(
     employee => {
-      dispatch(saveNewEmployee(employee));
+      if (employeeToEdit.id) {
+        dispatch(editEmployee({ ...employee, id: employeeToEdit.id }));
+        dispatch(resetEmployeeToEdit());
+      } else {
+        dispatch(saveNewEmployee(employee));
+      }
     },
-    [dispatch]
+    [dispatch, employeeToEdit]
   );
+
+  const initialValues = employeeToEdit
+    ? { ...employeeToEdit }
+    : {
+        firstName: "",
+        surname: "",
+        email: "",
+        birthDate: "",
+        status: "",
+        jobTitle: "",
+      };
+
+  useEffect(() => {
+    return () => {
+      if (employeeToEdit.id) {
+        dispatch(resetEmployeeToEdit());
+      }
+    };
+  }, [dispatch, employeeToEdit]);
 
   return (
     <>
-      <Header>Create new employee</Header>
+      <Header>
+        {employeeToEdit.id !== undefined
+          ? "Edit employee"
+          : "Create new employee"}
+      </Header>
       <Formik
         validationSchema={formValidationSchema}
         onSubmit={submitForm}
-        initialValues={{
-          firstName: "",
-          surname: "",
-          email: "",
-          birthday: "",
-          status: "",
-          jobTitle: "",
-        }}
+        initialValues={initialValues}
       >
         <Flex alignItems="center" justifyContent="center" height="100%">
           <Flex alignItems="left" direction="column" width="300px">
             <FormField name="firstName" placeholder="First name" />
             <FormField name="surname" placeholder="Surname" />
             <FormField name="email" placeholder="Email" />
-            <FormField name="birthday" placeholder="Birth Date (YYYY-MM-DD)" />
+            <FormField name="birthDate" placeholder="Birth Date (YYYY-MM-DD)" />
             <FormField name="status" placeholder="status" />
             <FormField name="jobTitle" placeholder="Job Title" />
             <FormButtons />
