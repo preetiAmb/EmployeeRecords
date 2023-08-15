60953b351e2ab91b42f2f1887a0cb4f76da84026
Preeti <preetijav@gmail.com>
2023-08-15 21:10:36 +0200

    Added required tests

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -8,6 +8,9 @@ describe("Examine the creation of employees", () => {
     cy.get("[data-cy=firstNameErrorMessage]").should("contain", "Required");
     cy.get("[data-cy=surnameErrorMessage]").should("contain", "Required");
     cy.get("[data-cy=emailErrorMessage]").should("contain", "Required");
+    cy.get("[data-cy=birthDateErrorMessage]").should("contain", "Required");
+    cy.get("[data-cy=statusErrorMessage]").should("contain", "Required");
+    cy.get("[data-cy=jobTitleErrorMessage]").should("contain", "Required");
   });
 
   it("validates too long submitted values properly", () => {
@@ -42,5 +45,26 @@ describe("Examine the creation of employees", () => {
       "contain",
       "Invalid email address"
     );
+  });
+
+  it("validates invalid birthDate format properly", () => {
+    cy.get("[data-cy=birthDateInput]").type("invalid-date-format");
+    cy.get("[data-cy=saveButton]").click();
+    cy.get("[data-cy=birthDateErrorMessage]").should(
+      "contain",
+      "Date must be in the format YYYY-MM-DD"
+    );
+  });
+  
+  it("created employee details successfully", () => {
+
+    cy.get("[data-cy=firstNameInput]").type("Abe");
+    cy.get("[data-cy=surnameInput]").type("Simpson");
+    cy.get("[data-cy=emailInput]").type("abe.simpson@springfield.com");
+    cy.get("[data-cy=birthDateInput]").type("1907-05-25");
+    cy.get("[data-cy=statusInput]").type("ACTIVE");
+    cy.get("[data-cy=jobTitleInput]").type("Work grouch");
+    
+    cy.get("[data-cy=saveButton]").click();
   });
 });
---
+++ cypress/e2e/edit.cy.js
@@ -0,0 +1,40 @@
+describe("Examine the editing of employees", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/#/edit/1692036973785");
+  });
+  it("validates empty submitted fields properly", () => {
+    cy.get("[data-cy=saveButton]").click();
+    cy.get("[data-cy=firstNameErrorMessage]").should("contain", "Required");
+    cy.get("[data-cy=surnameErrorMessage]").should("contain", "Required");
+    cy.get("[data-cy=emailErrorMessage]").should("contain", "Required");
+  });
+
+  it("edits employee details successfully", () => {
+    cy.get("[data-cy=firstNameInput]").clear().type("NewFirstName");
+    cy.get("[data-cy=surnameInput]").clear().type("NewSurname");
+    cy.get("[data-cy=emailInput]").clear().type("new.email@example.com");
+    cy.get("[data-cy=birthDateInput]").clear().type("1990-01-01");
+    cy.get("[data-cy=statusInput]").clear().type("ACTIVE");
+    cy.get("[data-cy=jobTitleInput]").clear().type("New Developer");
+
+    cy.get("[data-cy=saveButton]").click();
+  });
+
+  it("validates invalid email address during edit", () => {
+    cy.get("[data-cy=emailInput]").clear().type("invalid-email");
+    cy.get("[data-cy=saveButton]").click();
+    cy.get("[data-cy=emailErrorMessage]").should(
+      "contain",
+      "Invalid email address"
+    );
+  });
+
+  it("validates invalid birthDate format properly", () => {
+    cy.get("[data-cy=birthDateInput]").type("invalid-date-format");
+    cy.get("[data-cy=saveButton]").click();
+    cy.get("[data-cy=birthDateErrorMessage]").should(
+      "contain",
+      "Date must be in the format YYYY-MM-DD"
+    );
+  });
+});
