21a9a7fd5cd693f2a1d96a8d26291e83496c439a
Preeti <preetijav@gmail.com>
2023-08-23 09:03:39 +0200

    Added no employee found message

--- src/components/View/index.js
+++ src/components/View/index.js
@@ -73,54 +73,62 @@ const View = () => {
           </Button>
         </Box>
         <Box marginTop="md">
-          <Table data-cy="table">
-            <TableHeader data-cy="tableHeader">
-              <TableRow data-cy="tableRow">
-                <TableCell data-cy="tableCell">FirstName</TableCell>
-                <TableCell data-cy="tableCell">Surname</TableCell>
-                <TableCell data-cy="tableCell">Email</TableCell>
-                <TableCell data-cy="tableCell">Birth Date</TableCell>
-                <TableCell data-cy="tableCell">Status</TableCell>
-                <TableCell data-cy="tableCell">Job Title</TableCell>
-                <TableCell data-cy="tableCell">Actions</TableCell>
-              </TableRow>
-            </TableHeader>
-            <TableBody data-cy="tableBody">
-              {employees &&
-                employees.map(employee => (
-                  <TableRow key={employee.id} data-cy="tableRow">
-                    <TableCell data-cy="tableCell">
-                      {employee.firstName}
-                    </TableCell>
-                    <TableCell data-cy="tableCell">
-                      {employee.surname}
-                    </TableCell>
-                    <TableCell data-cy="tableCell">{employee.email}</TableCell>
-                    <TableCell data-cy="tableCell">
-                      {employee.birthDate}
-                    </TableCell>
-                    <TableCell data-cy="tableCell">{employee.status}</TableCell>
-                    <TableCell data-cy="tableCell">
-                      {employee.jobTitle}
-                    </TableCell>
-                    <TableCell data-cy="tableCell">
-                      <Button
-                        data-cy={`editButton-${employee.id}`}
-                        onClick={() => handleEdit(employee.id)}
-                      >
-                        Edit
-                      </Button>
-                      <Button
-                        data-cy={`deleteButton-${employee.id}`}
-                        onClick={() => handleDelete(employee.id)}
-                      >
-                        Delete
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
+          {employees.length > 0 ? (
+            <Table data-cy="table">
+              <TableHeader data-cy="tableHeader">
+                <TableRow data-cy="tableRow">
+                  <TableCell data-cy="tableCell">FirstName</TableCell>
+                  <TableCell data-cy="tableCell">Surname</TableCell>
+                  <TableCell data-cy="tableCell">Email</TableCell>
+                  <TableCell data-cy="tableCell">Birth Date</TableCell>
+                  <TableCell data-cy="tableCell">Status</TableCell>
+                  <TableCell data-cy="tableCell">Job Title</TableCell>
+                  <TableCell data-cy="tableCell">Actions</TableCell>
+                </TableRow>
+              </TableHeader>
+              <TableBody data-cy="tableBody">
+                {employees &&
+                  employees.map(employee => (
+                    <TableRow key={employee.id} data-cy="tableRow">
+                      <TableCell data-cy="tableCell">
+                        {employee.firstName}
+                      </TableCell>
+                      <TableCell data-cy="tableCell">
+                        {employee.surname}
+                      </TableCell>
+                      <TableCell data-cy="tableCell">
+                        {employee.email}
+                      </TableCell>
+                      <TableCell data-cy="tableCell">
+                        {employee.birthDate}
+                      </TableCell>
+                      <TableCell data-cy="tableCell">
+                        {employee.status}
+                      </TableCell>
+                      <TableCell data-cy="tableCell">
+                        {employee.jobTitle}
+                      </TableCell>
+                      <TableCell data-cy="tableCell">
+                        <Button
+                          data-cy={`editButton-${employee.id}`}
+                          onClick={() => handleEdit(employee.id)}
+                        >
+                          Edit
+                        </Button>
+                        <Button
+                          data-cy={`deleteButton-${employee.id}`}
+                          onClick={() => handleDelete(employee.id)}
+                        >
+                          Delete
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <p>No employee records found.</p>
+          )}
         </Box>
         <div ref={sentinelRef} style={{ height: "8px" }} />
       </Flex>
